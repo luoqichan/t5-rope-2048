166dfe738a6919452efbeac32fd009357e3fb360
luoqichan <chanluoqi@gmail.com>
2024-03-12 17:56:54 -0400

    add combine negatives

---
+++ scripts/experiments/bkt_combine_negatives.py
@@ -0,0 +1,37 @@
+import json
+from tqdm import tqdm
+from argparse import ArgumentParser
+
+
+
+parser = ArgumentParser()
+parser.add_argument('--negatives_dir', type=str, required=True)
+
+args = parser.parse_args()
+pdir = args.negatives_dir
+
+print("Loading hard negatives...")
+combined_negatives = {}
+count = 0
+with open(f"{pdir}/full.jsonl", "r") as hn:
+    for i in tqdm(hn):
+        line = json.loads(i)
+        k = "-".join([str(x) for x in line["query"]])
+        combined_negatives[k] = line
+
+print("Loading and combining cluster_negatives...")
+with open(f"{pdir}/full.cn.jsonl", "r") as cn:
+    for i in tqdm(cn): 
+        line = json.loads(i)
+        k = "-".join([str(x) for x in line["query"]])
+        combined_negatives[k]["negatives"][5:] = line["negatives"][:4]
+
+
+print("Writing and saving to file...")
+with open(f"{pdir}/full.collated.jsonl", "w") as wf:
+    for _, dat in tqdm(combined_negatives.items()):
+        json.dump(dat, wf)
+        wf.write("\n")
+
+
+
