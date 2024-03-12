e8857f1a3e6865d43bce2c5ff7c321e114bfd267
luoqichan <chanluoqi@gmail.com>
2024-03-12 17:14:56 -0400

    hn

--- scripts/hn.sh
+++ scripts/hn.sh
@@ -22,23 +22,19 @@ train_qrels=$DATA_PATH/data/marco_documents_processed/qrels.train.tsv
 train_queries=$DATA_PATH/data/marco_documents_processed/train.query.txt
 corpus=$DATA_PATH/data/marco_documents_processed/corpus_firstp_2048.tsv
 
-
 train_data_folder=$DATA_PATH/data/training_data/t5-rope-bkt-warmup
-
-train_data=$train_data_folder/train.jsonl
-valid_data=$train_data_folder/val.jsonl
 model_path=/data/user_data/luoqic/t5-rope-data/models/t5-rope-2048-marco-bkt
 
 
 python OpenMatch/scripts/msmarco/build_hn.py  \
     --tokenizer_name $model_path \
-    --hn_file $DATA_PATH/data/negatives/t5-rope-bkt-warmup/L1.CN.trec \
+    --hn_file $DATA_PATH/data/negatives/t5-rope-bkt-warmup/Llast.CN.trec \
     --qrels $train_qrels \
     --queries $train_queries  \
     --collection $corpus  \
-    --save_to $train_data_folder  \
+    --save_to $train_data_folder  \ 
     --doc_template "Title: <title> Text: <text>" \
-    --n_sample 9 \
+    --n_sample 5 \
     --truncate $text_length
 
 # python OpenMatch/scripts/msmarco/build_hn.py  \
@@ -52,6 +48,6 @@ python OpenMatch/scripts/msmarco/build_hn.py  \
 #     --n_sample 9 \
 #     --truncate $text_length
 
-# cat $train_data_folder/*.hn.jsonl > $train_data_folder/full.jsonl
-# rm $train_data_folder/*.hn.jsonl
+cat $train_data_folder/*.hn.jsonl > $train_data_folder/full-last.cn.jsonl
+rm $train_data_folder/*.hn.jsonl
 
