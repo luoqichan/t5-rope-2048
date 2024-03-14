39256dcb8f2ec8f58404676d521df67cd4bebe22
luoqichan <chanluoqi@gmail.com>
2024-03-14 18:40:57 -0400

    debug scripts

--- scripts/train_bkt.sh
+++ scripts/train_bkt.sh
@@ -5,30 +5,31 @@
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=200G
-#SBATCH --gres=gpu:A6000:4
-#SBATCH --time=7-00:00:00
+#SBATCH --gres=gpu:A6000:2
+#SBATCH --time=5:00:00
 
 eval "$(conda shell.bash hook)"
 conda activate openmatch
 
 
-# initial_model=$1
 
 split=documents
 text_length=2048
 n_gpus=2
-# num_episodes=4
 DATA_PATH=/data/user_data/luoqic/t5-rope-data
 train_qrels=$DATA_PATH/data/marco_documents_processed/qrels.train.tsv
 train_queries=$DATA_PATH/data/marco_documents_processed/train.query.txt
 corpus=$DATA_PATH/data/marco_documents_processed/corpus_firstp_2048.tsv
 
 initial_model=$DATA_PATH/models/t5-rope-2048-marco-bkt
-train_data_folder=$DATA_PATH/data/training_data/t5-rope-bkt-warmup
-train_data=$train_data_folder/train.jsonl
-valid_data=$train_data_folder/val.jsonl
+train_data_folder=$DATA_PATH/data/training_data/t5-base-marco-documents-2048
+# train_data=$train_data_folder/train.jsonl
+# valid_data=$train_data_folder/val.jsonl
 
-trained_model_name=t5-rope-bkt-warmup-clusterneg1
+train_data=$train_data_folder/split00.hn.jsonl
+valid_data=$train_data_folder/split00.hn.jsonl
+
+trained_model_name=t5-base-marco-documents-2048-debug
 output_path=$DATA_PATH/models/$trained_model_name
 
 accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/train_dr.py  \
