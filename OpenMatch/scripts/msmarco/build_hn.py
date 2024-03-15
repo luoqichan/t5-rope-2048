605b84027f411206f67dfd611fbf915db6ae165a
luoqichan <chanluoqi@gmail.com>
2024-03-15 16:34:40 -0400

    combined negatives script

--- OpenMatch/scripts/msmarco/build_hn.py
+++ OpenMatch/scripts/msmarco/build_hn.py
@@ -35,40 +35,6 @@ def load_ranking(rank_file, relevance, n_sample, depth, split_token):
                 random.shuffle(negatives)
                 yield curr_q, relevance[curr_q], negatives[:n_sample], split_token
                 return
-            
-#TODO: Loading multiple rankings? 
-def load_ranking_archive(rank_file, relevance, n_sample, depth, split_token):
-    with open(rank_file) as rf:
-        lines = iter(rf)
-        l_0 = next(lines).strip().split()
-        q_0, p_0 = l_0[0], l_0[1]
-
-        curr_q = q_0
-        negatives = [] if p_0 in relevance[q_0] else [p_0]
-        cluster_negatives = [[c] for c in l_0[2:]]
-
-        while True:
-            try:
-                l = next(lines).strip().split()
-                q, p, cn = l[0], l[1], l[2:]
-                if q != curr_q:
-                    negatives = negatives[:depth]
-                    random.shuffle(negatives)
-                    yield curr_q, relevance[curr_q], negatives[:n_sample], [c[:n_sample] for c in cluster_negatives],split_token
-                    curr_q = q
-                    negatives = [] if p in relevance[q] else [p]
-                    cluster_negatives = [[c] for c in cn]
-                else:
-                    if p not in relevance[q]:
-                        negatives.append(p)
-                    for idx, level in enumerate(cluster_negatives): 
-                        level.append(cn[idx])
-
-            except StopIteration:
-                negatives = negatives[:depth]
-                random.shuffle(negatives)
-                yield curr_q, relevance[curr_q], negatives[:n_sample], [c[:n_sample] for c in cluster_negatives], split_token
-                return
 
 def load_processed_ranking(rank_file, relevance, n_sample, depth, split_token):
     with open(rank_file) as rf:
--- scripts/hn.sh
+++ scripts/hn.sh
@@ -14,20 +14,20 @@ DATA_PATH="/data/user_data/luoqic/t5-rope-data"
 
 split=documents
 text_length=2048
-n_gpus=4
 
 
 train_qrels=$DATA_PATH/data/marco_documents_processed/qrels.train.tsv
 train_queries=$DATA_PATH/data/marco_documents_processed/train.query.txt
 corpus=$DATA_PATH/data/marco_documents_processed/corpus_firstp_2048.tsv
 
-train_data_folder=$DATA_PATH/data/training_data/t5-base-marco-documents-2048
+train_data_folder=$DATA_PATH/data/training_data/t5-base-marco-documents-2048-bkt
 model_path=/data/user_data/luoqic/t5-rope-data/models/t5-base-marco-documents-2048
 
+mkdir -p $train_data_folder
 
 python OpenMatch/scripts/msmarco/build_hn.py  \
     --tokenizer_name $model_path \
-    --hn_file $DATA_PATH/data/negatives/t5-base-marco-documents-2048/toy_negatives.txt \
+    --hn_file $DATA_PATH/data/negatives/t5-base-marco-documents-2048-bkt/combined_negatives.txt \
     --qrels $train_qrels \
     --queries $train_queries  \
     --collection $corpus  \
@@ -47,6 +47,14 @@ python OpenMatch/scripts/msmarco/build_hn.py  \
 #     --n_sample 9 \
 #     --truncate $text_length
 
-# cat $train_data_folder/*.hn.jsonl > $train_data_folder/full-last.cn.jsonl
-# rm $train_data_folder/*.hn.jsonl
+cat $train_data_folder/*.hn.jsonl > $train_data_folder/full.jsonl
+rm $train_data_folder/*.hn.jsonl
 
+line_count=$(wc -l $train_data_folder/full.jsonl | awk '{print $1}')
+n_val=500
+n_train=$((line_count - n_val))
+
+echo $n_train
+
+tail -n $n_val $train_data_folder/full.jsonl > $train_data_folder/val.jsonl
+head -n $n_train $train_data_folder/full.jsonl > $train_data_folder/train.jsonl
