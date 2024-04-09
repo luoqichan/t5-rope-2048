a0db4e1caebc7f2e39b05bcef1d4f2170d7ac4a3
luoqichan <chanluoqi@gmail.com>
2024-04-09 00:01:40 -0400

    hn for self-hn-1 /////

--- scripts/hn.sh
+++ scripts/hn.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=train_dr_openmatch
+#SBATCH --job-name=build_hn
 #SBATCH --output=logs/%x-%j.out
 #SBATCH -e logs/%x-%j.err
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=200G
-#SBATCH --gres=gpu:A6000:4
-#SBATCH --time=7-00:00:00
+#SBATCH --mem=128G
+#SBATCH --time=8:00:00
 
 eval "$(conda shell.bash hook)"
 conda activate openmatch
@@ -15,38 +14,20 @@ DATA_PATH="/data/user_data/luoqic/t5-rope-data"
 
 split=documents
 text_length=2048
-n_gpus=4
 
-num_episodes=4
 
 train_qrels=$DATA_PATH/data/marco_documents_processed/qrels.train.tsv
 train_queries=$DATA_PATH/data/marco_documents_processed/train.query.txt
 corpus=$DATA_PATH/data/marco_documents_processed/corpus_firstp_2048.tsv
-negatives=$DATA_PATH/data/marco_documents_processed/train.negatives.tsv
 
-initial_model=$1
-trained_model_name=t5-base-marco-$split-$text_length-self-hn-1
-
-
-echo "########################################"
-echo "Train + HN sampling loop - 4 episodes"
-echo "########################################"
-
-train_data=$train_data_folder/train.jsonl
-valid_data=$train_data_folder/val.jsonl
 output_path=/data/user_data/luoqic/t5-rope-data/models/t5-base-marco-documents-2048
 
 
-    # Hard negative sampling - ANCE negative refresh
-    # set variables for next training episode
-
-i=2
-run_save=$DATA_PATH/data/negatives/$trained_model_name
-
-trained_model_name=$trained_model_name-self-hn-$i
+trained_model_name=t5-base-marco-documents-2048-self-hn-1
 train_data_folder=$DATA_PATH/data/training_data/$trained_model_name
-
-mkdir -p $run_save
+train_data=$train_data_folder/train.jsonl
+valid_data=$train_data_folder/val.jsonl
+run_save=$DATA_PATH/data/negatives/$trained_model_name
 
 python OpenMatch/scripts/msmarco/build_hn.py  \
     --tokenizer_name $output_path \
