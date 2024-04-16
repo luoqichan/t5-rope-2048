7ad64f0fd274fd2e3c3f665fd0c8e05610ed96db
luoqichan <chanluoqi@gmail.com>
2024-04-16 11:21:38 -0400

    eval debug-eval

--- scripts/train_bkt.sh
+++ scripts/train_bkt.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=200G
 #SBATCH --gres=gpu:A6000:4
-#SBATCH --time=7-00:00:00
+#SBATCH --time=6:00:00
 
 eval "$(conda shell.bash hook)"
 conda activate openmatch
@@ -29,10 +29,10 @@ initial_model=$DATA_PATH/models/t5-base-marco-documents-2048
 # train_data_folder=$DATA_PATH/data/training_data/t5-base-marco-documents-2048-bkt
 # train_data=$train_data_folder/train.jsonl
 # valid_data=$train_data_folder/val.jsonl
-train_data=/compute/shire-1-6/luoqic/t5-rope/train.jsonl
-valid_data=/compute/shire-1-6/luoqic/t5-rope/val.jsonl 
+train_data=/compute/shire-1-6/luoqic/train.jsonl
+valid_data=/compute/shire-1-6/luoqic/val.jsonl 
 
-trained_model_name=t5-base-marco-documents-2048-HNCN-separatelosses-debug-hndata
+trained_model_name=t5-base-marco-documents-2048-HNCN-separatelosses-debug-eval
 output_path=$DATA_PATH/models/$trained_model_name
 
 # accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/train_dr.py  \
@@ -41,6 +41,7 @@ output_path=$DATA_PATH/models/$trained_model_name
 #     --do_train \
 #     --save_steps 125  \
 #     --eval_steps 125  \
+#     --save_total_limit 2 \
 #     --fp16 \
 #     --train_path $train_data  \
 #     --eval_path $valid_data  \
