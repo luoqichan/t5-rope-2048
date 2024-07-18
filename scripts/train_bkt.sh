9a6c9d58dc136d0c62671d1e812ec274c0a336f0
luoqichan <chanluoqi@gmail.com>
2024-07-17 22:03:41 -0400

    warmup

--- scripts/train_bkt.sh
+++ scripts/train_bkt.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=train_dr_openmatch
+#SBATCH --job-name=warmup
 #SBATCH --output=logs/%x-%j.out
 #SBATCH -e logs/%x-%j.err
-#SBATCH --exclude=babel-4-28
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128G
@@ -29,13 +28,13 @@ corpus=$DATA_PATH/data/marco_documents_processed/corpus_firstp_2048.tsv
 negatives=$DATA_PATH/data/marco_documents_processed/train.negatives.tsv
 
 initial_model=$DATA_PATH/models/t5-base-marco-documents-2048
-trained_model_name=t5-base-marco-$split-$text_length-HNCN-separatelosses-hn1data
+trained_model_name=t5-base-marco-$split-$text_length-warmup74
 
-# train_data_folder=$DATA_PATH/data/training_data/t5-base-marco-documents-2048-self-hn-1
-# train_data=$train_data_folder/train.jsonl
-# valid_data=$train_data_folder/val.jsonl
-train_data=/compute/babel-4-7/luoqic/t5-rope-hncn-updated/train.jsonl
-valid_data=/compute/babel-4-7/luoqic/t5-rope-hncn-updated/val.jsonl
+train_data_folder=$DATA_PATH/data/training_data/t5-base-marco-documents-2048
+train_data=$train_data_folder/train.jsonl
+valid_data=$train_data_folder/val.jsonl
+# train_data=/compute/babel-4-7/luoqic/t5-rope-hncn-updated/train.jsonl
+# valid_data=/compute/babel-4-7/luoqic/t5-rope-hncn-updated/val.jsonl
 
 output_path=$DATA_PATH/models/$trained_model_name
 
@@ -44,9 +43,9 @@ accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 
     --model_name_or_path $initial_model \
     --save_total_limit 2 \
     --do_train \
-    --save_steps 125  \
-    --max_steps 2 \
-    --eval_steps 125  \
+    --save_steps 74  \
+    --max_steps 74 \
+    --eval_steps 74  \
     --fp16 \
     --train_path $train_data  \
     --eval_path $valid_data  \
@@ -70,66 +69,66 @@ accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 
     --negatives_x_device True \
     --data_cache_dir /scratch/luoqic
 
-# embeddings_out=$DATA_PATH/data/embeddings/train/$trained_model_name
-# run_save=$DATA_PATH/data/negatives/$trained_model_name
+embeddings_out=$DATA_PATH/data/embeddings/train/$trained_model_name
+run_save=$DATA_PATH/data/negatives/$trained_model_name
 
-# mkdir -p $run_save
-# mkdir -p $embeddings_out
+mkdir -p $run_save
+mkdir -p $embeddings_out
 
-# accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/build_index.py  \
-#     --output_dir $embeddings_out \
-#     --model_name_or_path $output_path  \
-#     --per_device_eval_batch_size 430 \
-#     --corpus_path $corpus  \
-#     --doc_template "Title: <title> Text: <text>"  \
-#     --doc_column_names id,title,text  \
-#     --p_max_len $text_length  \
-#     --fp16  \
-#     --dataloader_num_workers 1
+accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/build_index.py  \
+    --output_dir $embeddings_out \
+    --model_name_or_path $output_path  \
+    --per_device_eval_batch_size 430 \
+    --corpus_path $corpus  \
+    --doc_template "Title: <title> Text: <text>"  \
+    --doc_column_names id,title,text  \
+    --p_max_len $text_length  \
+    --fp16  \
+    --dataloader_num_workers 1
 
-# accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/retrieve.py  \
-#     --output_dir $embeddings_out  \
-#     --model_name_or_path $output_path  \
-#     --per_device_eval_batch_size 600  \
-#     --query_path $train_queries  \
-#     --query_template "<text>"  \
-#     --query_column_names id,text  \
-#     --q_max_len 32  \
-#     --fp16  \
-#     --trec_save_path $run_save/train.trec \
-#     --dataloader_num_workers 1 \
-#     --use_gpu
+accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/retrieve.py  \
+    --output_dir $embeddings_out  \
+    --model_name_or_path $output_path  \
+    --per_device_eval_batch_size 600  \
+    --query_path $train_queries  \
+    --query_template "<text>"  \
+    --query_column_names id,text  \
+    --q_max_len 32  \
+    --fp16  \
+    --trec_save_path $run_save/train.trec \
+    --dataloader_num_workers 1 \
+    --use_gpu
 
-# model_to_eval=$output_path
-# model_name=$(basename "$model_to_eval")
+model_to_eval=$output_path
+model_name=$(basename "$model_to_eval")
 
-# embeddings_out="$DATA_PATH/data/embeddings/dev/$model_name"
-# run_save="$DATA_PATH/results/$model_name"
+embeddings_out="$DATA_PATH/data/embeddings/dev/$model_name"
+run_save="$DATA_PATH/results/$model_name"
 
-# dev_queries=$DATA_PATH/data/marco_documents_processed/dev.query.txt
-# dev_qrels=$DATA_PATH/data/marco_documents_processed/qrels.dev.tsv
+dev_queries=$DATA_PATH/data/marco_documents_processed/dev.query.txt
+dev_qrels=$DATA_PATH/data/marco_documents_processed/qrels.dev.tsv
 
-# mkdir -p $run_save
-# mkdir -p $embeddings_out
+mkdir -p $run_save
+mkdir -p $embeddings_out
 
-# # already have corpus embeddings from hard negatives
-# cp $DATA_PATH/data/embeddings/train/$model_name/embeddings.corpus.* $DATA_PATH/data/embeddings/dev/$model_name
+# already have corpus embeddings from hard negatives
+cp $DATA_PATH/data/embeddings/train/$model_name/embeddings.corpus.* $DATA_PATH/data/embeddings/dev/$model_name
 
-# accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/retrieve.py  \
-#     --output_dir $embeddings_out  \
-#     --model_name_or_path $model_to_eval \
-#     --per_device_eval_batch_size 600  \
-#     --query_path $dev_queries \
-#     --query_template "<text>"  \
-#     --query_column_names id,text  \
-#     --q_max_len 32  \
-#     --fp16  \
-#     --trec_save_path $run_save/dev.trec \
-#     --dataloader_num_workers 1 \
-#     --use_gpu \
-#     --retrieve_depth 100 \
-#     --rope True
+accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/retrieve.py  \
+    --output_dir $embeddings_out  \
+    --model_name_or_path $model_to_eval \
+    --per_device_eval_batch_size 600  \
+    --query_path $dev_queries \
+    --query_template "<text>"  \
+    --query_column_names id,text  \
+    --q_max_len 32  \
+    --fp16  \
+    --trec_save_path $run_save/dev.trec \
+    --dataloader_num_workers 1 \
+    --use_gpu \
+    --retrieve_depth 100 \
+    --rope True
 
-# python OpenMatch/scripts/evaluate.py $dev_qrels $run_save/dev.trec > $run_save/dev.results
+python OpenMatch/scripts/evaluate.py $dev_qrels $run_save/dev.trec > $run_save/dev.results
 
-# rm -r $DATA_PATH/data/embeddings/dev/$model_name/embeddings.corpus.*
+rm -r $DATA_PATH/data/embeddings/dev/$model_name/embeddings.corpus.*
